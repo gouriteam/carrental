4b34eeee5d13cc2df242e01b188dfef053927e07
GOURI KULKARNI <gourikulkarni4444@gmail.com>
2021-01-09 16:18:09 +0530

    hi

---
+++ project/crazy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project
+{
+    public class crazy
+    {
+        public void hi()
+        {
+            Console.WriteLine("hey team");
+        }
+    }
+}
